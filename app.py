0413528049c8f02ad537be653bf401eb06b7041a
osamakhurma <osamakhurma98@gmail.com>
2025-03-15 21:04:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ def players():
     return jsonify(get_names())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # استخدم المنفذ الذي توفره Render
+    app.run(host="0.0.0.0", port=port, debug=True)
