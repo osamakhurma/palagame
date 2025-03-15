37b008b074ac22e85a54d5b9f555db44594e1344
osamakhurma <osamakhurma98@gmail.com>
2025-03-15 21:32:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def save_player_name(name):
         writer.writerow([name])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # استخدم المنفذ الذي توفره Render
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
