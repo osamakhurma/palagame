4f1bd60a23393e0379400ef3fbfffbc9da8e5ba2
osamakhurma <osamakhurma98@gmail.com>
2025-03-15 21:17:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def get_question():
     return jsonify({"question": f"أين تقع مدينة {city}؟", "city": city, "coords": cities[city]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # استخدم المنفذ الذي توفره Render
+    app.run(host="0.0.0.0", port=port, debug=True)
